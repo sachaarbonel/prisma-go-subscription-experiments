3d54c91ecf515ccc0b17620df6a6bda9068bfe3a
Sach97 <sacha.arbonel@hotmail.fr>
2018-10-17 19:13:26 +0200

    websocket: bad handshake

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"log"
 	"net/http"
-	"time"
 
-	"github.com/gorilla/websocket"
+	websocket "github.com/gorilla/websocket"
 )
 
 type PayloadStruct struct {
@@ -22,22 +19,6 @@ type ResponseStruct struct {
 	} `json:"data"`
 }
 
-// Call represents an active request
-type Call struct {
-	Req   PayloadStruct
-	Res   ResponseStruct
-	Done  chan bool
-	Error error
-}
-
-func NewCall(req PayloadStruct) *Call {
-	done := make(chan bool)
-	return &Call{
-		Req:  req,
-		Done: done,
-	}
-}
-
 type WSClient struct {
 	conn *websocket.Conn
 }
@@ -46,80 +27,37 @@ func New() *WSClient {
 	return &WSClient{}
 }
 
-func (c *WSClient) read() {
-	for {
-		var res ResponseStruct
-		err := c.conn.ReadJSON(&res)
-		log.Printf("Receive : %v", res)
-		if err != nil {
-			log.Printf("error : %v", err)
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
-				log.Printf("error : %v", err)
-			}
-			break
-		}
-	}
-}
-
 func (c *WSClient) Connect(url string) error {
-	conn, _, err := websocket.DefaultDialer.Dial(url, http.Header{
-		"User-Agent": []string{"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"},
-	})
+
+	//do better than strings
+	headers := http.Header{
+		"Host":                     []string{"localhost:4466"},
+		"User-Agent":               []string{"Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0"},
+		"Accept":                   []string{"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"},
+		"Accept-Language":          []string{"en-US,en;q=0.5"},
+		"Accept-Encoding":          []string{"gzip, deflate"},
+		"Sec-WebSocket-Version":    []string{"13"},
+		"Origin":                   []string{"http://localhost:4466"},
+		"Sec-WebSocket-Protocol":   []string{"graphql-ws"},
+		"Sec-WebSocket-Extensions": []string{"permessage-deflate"},
+		"Sec-WebSocket-Key":        []string{"/U1W39A3aqbOjTV7yFUyhg=="},
+		"Pragma":                   []string{"no-cache"},
+		"Cache-Control":            []string{"no-cache"},
+	}
+	conn, _, err := websocket.DefaultDialer.Dial(url, headers)
 	if err != nil {
 		return err
 	}
 	c.conn = conn
-	go c.read()
+	//go c.read()
 	return nil
-}
-
-func (c *WSClient) Request(payload interface{}) (interface{}, error) {
-
-	req := payload.(PayloadStruct)
-	call := NewCall(req)
-
-	err := c.conn.WriteJSON(&req)
-	if err != nil {
-		return nil, err
-	}
-
-	select {
-	case <-call.Done:
-	case <-time.After(2 * time.Second):
-		call.Error = errors.New("request timeout")
-	}
-
-	if call.Error != nil {
-		return nil, call.Error
-	}
-	return call.Res.Data, nil
-}
-
-func (c *WSClient) Close() error {
-	return c.conn.Close()
 }
 
 func main() {
 	client := New()
-	err := client.Connect("ws://localhost:4466/")
+	err := client.Connect("ws://localhost:4466")
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 
-	go func() {
-		want := PayloadStruct{
-			Query: "query {users {id }}",
-		}
-		_, err := client.Request(want)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}()
-
-	defer func() {
-		err = client.Close()
-		if err != nil {
-			panic(err)
-		}
-	}()
 }
